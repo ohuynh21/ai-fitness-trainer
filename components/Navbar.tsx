409333325988c2ad06ba61ab26ee432a2387da0b
ohuynh21 <100678197+ohuynh21@users.noreply.github.com>
2024-03-25 17:09:49 -0700

    finish navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const routes = [
+    {
+        name: 'Chat',
+        path: '/'
+    },
+    {
+        name: 'Profile',
+        path: '/profile'
+    }
+]
+
+function Navbar() {
+    const pathname = usePathname();
+    return ( 
+        <div className  = 'p-4 flex flex-row justify-between items-center bg-black text-white'>
+            <Link href = '/'>
+                <h1 className = 'text-2xl font-bold'> AI Personal Trainer</h1>
+            </Link>
+            <div className = 'flex gap-x-6 text-lg items-center'>
+                {routes.map((route, idx) => (
+                    <Link
+                    key = {idx}
+                    href = {route.path}
+                    // conditional styling
+                    className = {pathname === route.path ? 'border-b-2 border-yellow-500': ''}
+                    >
+                    {route.name}           
+                    </Link>
+                ))}
+                <UserButton afterSignOutUrl="/"/>
+            </div>
+        </div>
+     );
+}
+
+export default Navbar;
